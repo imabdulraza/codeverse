4bb5ac80e8a3682539cf5b5e48ecea4f3a0b37d5
Abdul Raza <imabdulraza@gmail.com>
2023-10-02 13:23:48 +0530

    added header footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import {increment, decrement} from './Redux/Actions/actions'
 import { ButtonGroup, Button } from '@mui/material';
+import { Header } from './Layouts/Header';
+import Footer from './Layouts/Footer';
 
 function App() {
   // state managed by redux
@@ -9,7 +11,8 @@ function App() {
   const isLogged = useSelector((state: any) => state.isLogged);
   const dispatch = useDispatch();
   return (
-    <div className="App">
+    <div>
+      <Header />
       <header className="App-header">
         <h2>codeverse</h2>
           <ButtonGroup variant="outlined" aria-label="outlined button group">
@@ -19,6 +22,7 @@ function App() {
           </ButtonGroup>
         { isLogged ? <p>welcome user</p> : ""}
       </header>
+      <Footer />
     </div>
   );
 }
